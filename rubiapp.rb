c3cbbded17eb8bdefe86e97b5006cfa2e5690f96
Desi <facebookaa@abv.bg>
2014-04-02 00:03:18 +0300

    fixed mess

---
+++ rubiapp.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# encoding: utf-8
+puts '<!DOCTYPE html>
+  <html>
+<head>
+        
+<title>Aloha Friday</title>
+</head>
+
+<body>
+<header>
+    <div class="languages">
+
+        <ul>
+        <li><a href="bg/index.html">bg</a></li>
+        <li><a href="en/index.html">en</a></li>
+        </ul>
+    </div>
+    <h1 class="pageTitle"><b>ALOHA</b>ta</h1>
+  ласкдйслдкфйдсилк
+<div class="image title">
+<h2>Happy days</h2>
+</div>
+
+
+</div>
+</header>
+'
